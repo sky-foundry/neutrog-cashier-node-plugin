139f44cee1786c7ac5e1132d560ee574e2437a08
Matthew Attanasio <matthew@skyfoundry.com.au>
2019-09-16 12:29:05 +0930

    chore: seeing if this works

--- src/utils/handleInitializeCheckout.ts
+++ src/utils/handleInitializeCheckout.ts
@@ -1,14 +1,26 @@
 import { Request } from 'express'
 
 const handleInitializeCheckout = (req: Request) => {
-  console.log('inital checkout req', req.body)
+  console.log('Initialized Checkout event recieved', req.body)
 
   return [
     {
       // data: req.body.custom_data,
       data: {
+        address: '50 Fultz Blvd',
+        address2: 'Another Address Line',
+        city: 'Winnipeg',
+        company: 'Bold Commerce Ltd',
+        country: 'Canada',
+        country_code: 'CA',
+        different_billing_address: true,
         first_name: 'John',
         last_name: 'Doe',
+        phone: '204-678-9087',
+        postal_code: 'R3Y 0L6',
+        province: 'Manitoba',
+        province_code: 'MB',
+        update_billing: true,
       },
       type: 'CHANGE_SHIPPING_ADDRESS',
     },
