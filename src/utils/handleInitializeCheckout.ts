298141b87a802af6ebfa9b076b38115e1388f5a8
Ari Seyhun <ariseyhun@live.com.au>
2019-09-19 11:27:50 +0930

    feat: add real shipping address to update

--- src/utils/handleInitializeCheckout.ts
+++ src/utils/handleInitializeCheckout.ts
@@ -1,25 +1,42 @@
 import { Request } from 'express'
 
+interface PickupAddress {
+  address: string
+  city: string
+  country: string
+  name: string
+  province: string
+  storeId: string
+  zipCode: string
+}
+
 const handleInitializeCheckout = (req: Request) => {
-  console.log('Initialized Checkout event recieved', JSON.stringify(req.body))
+  // console.log('Initialized Checkout event recieved', JSON.stringify(req.body))
+  const pickupAddress: PickupAddress = JSON.parse(
+    req.body.attributes.addressJson
+  )
+
+  console.log('')
+  console.log('PICKUP ADDRESS RECEIVED:    ', JSON.stringify(pickupAddress))
+  console.log('')
 
   return [
     {
       // data: req.body.custom_data,
       data: {
-        address: '50 Fultz Blvd',
-        address2: 'Another Address Line',
-        city: 'Winnipeg',
-        company: 'Bold Commerce Ltd',
-        country: 'Canada',
-        country_code: 'CA',
+        address: pickupAddress.address,
+        // address2: 'Another Address Line',
+        city: pickupAddress.city,
+        // company: 'Bold Commerce Ltd',
+        country: pickupAddress.country,
+        // country_code: 'CA',
         different_billing_address: true,
-        first_name: 'John',
-        last_name: 'Doe',
-        phone: '204-678-9087',
-        postal_code: 'R3Y 0L6',
-        province: 'Manitoba',
-        province_code: 'MB',
+        first_name: pickupAddress.name,
+        // last_name: 'Doe',
+        // phone: '204-678-9087',
+        postal_code: pickupAddress.zipCode,
+        // province: 'Manitoba',
+        province_code: pickupAddress.province,
         update_billing: true,
       },
       type: 'CHANGE_SHIPPING_ADDRESS',
