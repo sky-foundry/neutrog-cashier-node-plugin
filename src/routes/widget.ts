2a41bc753c63ff72029faa03f12051c9f337c47a
Ari Seyhun <ariseyhun@live.com.au>
2019-09-19 17:37:55 +0930

    fix: widget path

--- src/routes/widget.ts
+++ src/routes/widget.ts
@@ -1,7 +1,8 @@
 import { RequestHandler } from 'express'
+import path from 'path'
 
 const handler: RequestHandler = (req, res) => {
-  res.sendFile('../views/widget.html', { root: __dirname })
+  res.sendFile('views/widget.html', { root: path.join(__dirname, '..') })
 }
 
 export default handler
