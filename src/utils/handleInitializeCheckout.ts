3e8692e163146a8d5b4bf756c1ebe8ccbca14f80
Ari Seyhun <ariseyhun@live.com.au>
2019-09-23 11:25:06 +0930

    chore: cleanup console logs

--- src/utils/handleInitializeCheckout.ts
+++ src/utils/handleInitializeCheckout.ts
@@ -33,13 +33,6 @@ const states = {
 }
 
 const handleInitializeCheckout = (req: Request) => {
-  console.log('REQUEST BODY:')
-  console.log(JSON.stringify(req.body))
-
-  // console.log('')
-  // console.log('PICKUP ADDRESS RECEIVED:    ', req.body)
-  // console.log('')
-
   const pickupAddress: PickupAddress = JSON.parse(
     req.body.cart.attributes.Pickup_Store_JSON
   )
